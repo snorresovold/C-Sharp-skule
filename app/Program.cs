d419ac54ca69aa85766cc134a60f4762ee5a864e
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2022-10-03 13:10:21 +0200

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -22,37 +22,45 @@ namespace MyApplication
     {
         static void Main(string[] args)
         {
-            // Array som skal holde alle elever
+            // tomt array som skal holde alle elevene
             Student[] students = new Student[] { };
-
-            //Lager en ny elev og legger de i et array
-            Student Snorre = new Student("Snorre", 4.75, 23);
-            students.Append(Snorre);
-            Console.WriteLine(students);
-            // kjører IO funksjonen
-            IO();
-        }
-        // funksjon som har all konsoll logikken
-        static void IO()
-        {
             string input;
-            // infinite loop som programmet kjøres i
+            // infinite loop som programmet kjøres i ganske standard for konsoll applikasjoner
             while (true)
             {
+                // bare basic consol commands
                 Console.WriteLine("Hello, welcome to Snorre's Student Administrative system");
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("1: Get all students");
                 Console.WriteLine("2: add a new student");
                 input = Console.ReadLine();
+
             }
         }
+        // funksjon som har all konsoll logikken
+
         static void get_students()
         {
 
         }
         static void add_student()
         {
-            
+            //initialiserer variablene så eg kan gi de verdier seinere
+            string name;
+            double grades;
+            int absence;
+            // viser tekst og lagrer elevens navn
+            Console.WriteLine("What is the students name?");
+            name = Console.ReadLine();
+
+            // viser tekst i konsollen og konverterer til double, vil få error hvis input ikkje er numerisk
+            Console.WriteLine("What is the students grades? (has to be numbers)");
+            grades = Convert.ToDouble(Console.ReadLine());
+
+            // viser tekst og gjer det samme som koden over men med Ints, ikkje doubles
+            Console.WriteLine("how many minutes has the student been absent? (has to be numbers)");
+            absence = Int32.Parse(Console.ReadLine());
+            return name, grades, absence
         }
     }
 }
